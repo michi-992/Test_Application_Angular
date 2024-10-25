ed70c9a7165a4b21941a8d1cc0908c84de7c63b8
michi-992 <152781506+michi-992@users.noreply.github.com>
2024-10-25 13:48:30 +0200

    changed search-input to search-form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,13 +2,13 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { InfoBoxComponent } from './components/info-box/info-box.component';
 import { UserComponent } from './components/user/user.component';
-import { SearchInputComponent } from './components/search-input/search-input.component';
+import { SearchFormComponent } from './components/search-form/search-form.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NgbModule, InfoBoxComponent, UserComponent, SearchInputComponent],
+  imports: [RouterOutlet, NgbModule, InfoBoxComponent, UserComponent, SearchFormComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/components/search-form/search-form.component.spec.ts
+++ src/app/components/search-form/search-form.component.spec.ts
@@ -1,18 +1,18 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { SearchInputComponent } from './search-input.component';
+import { SearchFormComponent } from './search-form.component';
 
-describe('SearchInputComponent', () => {
-  let component: SearchInputComponent;
-  let fixture: ComponentFixture<SearchInputComponent>;
+describe('SearchFormComponent', () => {
+  let component: SearchFormComponent;
+  let fixture: ComponentFixture<SearchFormComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [SearchInputComponent]
+      imports: [SearchFormComponent]
     })
     .compileComponents();
 
-    fixture = TestBed.createComponent(SearchInputComponent);
+    fixture = TestBed.createComponent(SearchFormComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
