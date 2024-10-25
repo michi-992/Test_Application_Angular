c4a5788b8580d13c599f3c7e9ae3bdc9ed0db0c6
michi-992 <152781506+michi-992@users.noreply.github.com>
2024-10-25 12:01:09 +0200

    added user component to display

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { InfoBoxComponent } from './components/info-box/info-box.component';
+import { UserComponent } from './components/user/user.component';
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, InfoBoxComponent],
+  imports: [RouterOutlet, InfoBoxComponent, UserComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
