456c860701c7a56d96ebc0bab3c03ae6b53ff05c
michi-992 <152781506+michi-992@users.noreply.github.com>
2024-10-25 11:54:31 +0200

    added user service

---
+++ src/app/services/user/user.service.ts
@@ -0,0 +1,12 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http'
+import { Observable } from 'rxjs';
+import { User } from '../../models/user/user.model'
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserService {
+  constructor() { }
+}
