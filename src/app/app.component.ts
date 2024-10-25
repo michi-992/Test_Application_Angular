412776373a1aa2bc2c59d9a923ba7edbe2705e71
michi-992 <152781506+michi-992@users.noreply.github.com>
2024-10-25 13:30:35 +0200

    added bootstrap

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,12 +3,12 @@ import { RouterOutlet } from '@angular/router';
 import { InfoBoxComponent } from './components/info-box/info-box.component';
 import { UserComponent } from './components/user/user.component';
 import { SearchInputComponent } from './components/search-input/search-input.component';
-
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, InfoBoxComponent, UserComponent, SearchInputComponent],
+  imports: [RouterOutlet, NgbModule, InfoBoxComponent, UserComponent, SearchInputComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
