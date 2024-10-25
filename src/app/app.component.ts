150ca35696935eededa346df2cbc012dc3d9da70
michi-992 <152781506+michi-992@users.noreply.github.com>
2024-10-25 12:31:03 +0200

    added search input component (default)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,12 +2,13 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { InfoBoxComponent } from './components/info-box/info-box.component';
 import { UserComponent } from './components/user/user.component';
+import { SearchInputComponent } from './components/search-input/search-input.component';
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, InfoBoxComponent, UserComponent],
+  imports: [RouterOutlet, InfoBoxComponent, UserComponent, SearchInputComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
