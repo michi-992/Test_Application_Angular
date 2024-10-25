58184c2c6320cd330d8ac26126e94ae23251e2a8
michi-992 <152781506+michi-992@users.noreply.github.com>
2024-10-25 12:00:54 +0200

    added user service to user component

--- src/app/components/user/user.component.ts
+++ src/app/components/user/user.component.ts
@@ -1,12 +1,19 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { UserService } from '../../services/user/user.service'
+import { User } from '../../models/user/user.model'
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-user',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './user.component.html',
   styleUrl: './user.component.css'
 })
 export class UserComponent {
-  constructor() { }
+  user$: Observable<User>;
+  constructor(private userService: UserService) {
+    this.user$ = this.userService.getUser();
+  }
 }
