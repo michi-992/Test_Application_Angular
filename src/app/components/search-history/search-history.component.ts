19c516d41cfc1582bed9d8565e1d8bbdc073bfed
michi-992 <152781506+michi-992@users.noreply.github.com>
2024-10-26 13:31:51 +0200

    created search history component

---
+++ src/app/components/search-history/search-history.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-search-history',
+  standalone: true,
+  imports: [],
+  templateUrl: './search-history.component.html',
+  styleUrl: './search-history.component.css'
+})
+export class SearchHistoryComponent {
+  constructor() { }
+}
